789ee2ad51acf88aec61841bb2f5f2214b669d68
mpxfactor <tancmpsinc@gmail.com>
2023-11-26 12:09:29 +0530

    added controller for question and also added proper routes

--- src/routes/question.route.js
+++ src/routes/question.route.js
@@ -1,23 +1,21 @@
 import express from "express";
 import { isAdmin, verifyToken } from "../middlewares/auth.middleware.js";
 import {
-  addQuestion,
   deleteQuestionById,
-  getAllQuestions,
-  getQuestionsByQuizId,
   updateQuestionById,
   getQuestionById,
-  getQuestionsByQuizIdWithChoices,
+  addQuestionById,
 } from "../controllers/question.controller.js";
 
 const router = express.Router();
 
-router.get("/getallquestions", verifyToken, getAllQuestions);
-router.get("/:quizId", verifyToken, getQuestionsByQuizId);
-router.get("/allquestions/:id", getQuestionsByQuizIdWithChoices)
-router.get("/:id", verifyToken, getQuestionById);
-router.post("/", [verifyToken, isAdmin], addQuestion);
-router.put("/:id", [verifyToken, isAdmin], updateQuestionById);
-router.delete("/:id", [verifyToken, isAdmin], deleteQuestionById);
+router.get("/:quizId/:questionId", verifyToken, getQuestionById);
+router.post("/:quizId", [verifyToken, isAdmin], addQuestionById);
+router.put("/:quizId/:questionId", [verifyToken, isAdmin], updateQuestionById);
+router.delete(
+  "/:quizId/:questionId",
+  [verifyToken, isAdmin],
+  deleteQuestionById
+);
 
 export default router;
